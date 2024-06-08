74b8cb9424d5ceb8e778c5d853b5851880017c7b
arnold buiuca <arnoldbuiuka@gmail.com>
2024-06-08 17:05:16 +0000

    fix the backend for conf message

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -15,17 +18,20 @@ def prislista():
     return render_template("prislista.html", page_title="Prislista")
 
 
-@app.route("/kontakt", methods=["GET", "POST"])
-def kontakt():
-    if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
-    return render_template("kontakt.html", page_title="Kontakt")
-
-
 @app.route("/about")
 def about():
     return render_template("about.html", page_title="Allt om vår frisersalong")
+
+
+@app.route("/kontakt", methods=["GET", "POST"])
+def kontakt():
+    if request.method == "POST":
+        flash("Tack {}, vi har tagit emot ditt meddelande!".format(
+            request.form.get("name")))
+    return render_template("kontakt.html", page_title="Kontakt")
+
+
+
 
 
 if __name__ == "__main__":
